86d91b5b71742b567f409e911084b6b95f01648e
guanqun <guanqun.lu@gmail.com>
2012-08-23 07:28:28 +0800

    HTTP Handlers

---
+++ http-handlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type String string
+
+type Struct struct {
+	Greeting string
+	Punct	 string
+	Who 	 string
+}
+
+func (h String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, h)
+}
+
+func (h Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, h.Greeting, h.Punct, h.Who)
+}
+
+func main() {
+	http.Handle("/string", String("I'm Guanqun"))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+	http.ListenAndServe("localhost:4000", nil)
+}
